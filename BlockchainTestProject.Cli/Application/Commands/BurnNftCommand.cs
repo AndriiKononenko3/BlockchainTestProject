beade31308802adb1de5fc5720dae8652e998ccf
AndriiKononenko3 <47975862+AndriiKononenko3@users.noreply.github.com>
2022-10-03 21:40:48 +0300

    Updated BurnNftCommandHandler

--- BlockchainTestProject.Cli/Application/Commands/BurnNftCommand.cs
+++ BlockchainTestProject.Cli/Application/Commands/BurnNftCommand.cs
@@ -1,6 +1,6 @@
 using BlockchainTestProject.Application.Models;
+using BlockchainTestProject.Domain.Aggregates;
 using BlockchainTestProject.Persistence;
-using LanguageExt.UnsafeValueAccess;
 using MediatR;
 using OneOf;
 
@@ -23,19 +23,19 @@ public class BurnNftCommandHandler : IRequestHandler<BurnNftCommand.Request, One
     public async Task<OneOf<bool, string>> Handle(BurnNftCommand.Request request, CancellationToken cancellationToken)
     {
         var walletOption = await _walletRepository.GetNftAsync(request.Command.TokenId);
-        if (walletOption.IsNone)
-        {
-            return "Wallet not found";
-        }
-        
-        var wallet = walletOption.ValueUnsafe();
-        var (success, error) = wallet.RemoveNft(request.Command.TokenId);
-        if (!success)
-        {
-            return error; // TODO: return error type not string
-        }
 
-        await _walletRepository.SaveWalletAggregateAsync(wallet);
-        return true;
+        return await walletOption.MatchAsync<Wallet, OneOf<bool, string>>(
+            async wallet =>
+            {
+                var (success, error) = wallet.RemoveNft(request.Command.TokenId);
+                if (!success)
+                {
+                    return error; // TODO: return error type not string
+                }
+
+                await _walletRepository.SaveWalletAggregateAsync(wallet);
+                return true;
+            },
+            () => "Wallet not found");
     }
 }
